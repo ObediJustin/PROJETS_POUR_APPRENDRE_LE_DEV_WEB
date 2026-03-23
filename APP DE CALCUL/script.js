00ce5f22eed96926f1de70ab406c4f9a8b9c837a
ObediJustin <sadikiobedi0@gmail.com>
2026-03-23 22:28:02 +0200

    suite de l'app de calcul, Correction du fichier js

--- APP DE CALCUL/script.js
+++ APP DE CALCUL/script.js
@@ -1,11 +1,11 @@
 let query = document.querySelector(".query");
 let searchBtn = document.querySelector("button");
-let result = document.querySelector(".response"); 
+let response = document.querySelector(".response"); 
 
 searchBtn.onclick = function(){
     if(query.value.length > 0){
-        result.value = "resultat: " + eval (query.value);
-        result.style.display = "block";
+        response.value = "resultat: " + eval (query.value);
+        response.style.display = "block";
     } else {
         alert("champ vide");
         response.style.display = "none";
